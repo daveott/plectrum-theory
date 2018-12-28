340319feac96f40ca95af8d30a576cbbd6becbb2
Dave Ott <david.t.ott@gmail.com>
2018-12-28 16:23:50 -0500

    Spell D Major

--- spec/plectrum/theory/scale_spec.rb
+++ spec/plectrum/theory/scale_spec.rb
@@ -1,22 +1,26 @@
 RSpec.describe Plectrum::Theory::Scale do
   describe '#spell' do
     context 'C Major' do
-      context 'with a bitmask' do
-        subject { described_class.new(root: 'C', number: 2741) }
+      subject { described_class.new(root: 'C', number: 2741) }
 
-        it 'spells the scale' do
-          expect(subject.spell).to eq(%w(C D E F G A B))
-        end
+      it 'spells the scale' do
+        expect(subject.spell).to eq(%w(C D E F G A B))
       end
     end
 
     context 'G Major' do
-      context 'with a bitmask' do
-        subject { described_class.new(root: 'G', number: 2741) }
+      subject { described_class.new(root: 'G', number: 2741) }
 
-        it 'spells the scale' do
-          expect(subject.spell).to eq(%w(G A B C D E F#))
-        end
+      it 'spells the scale' do
+        expect(subject.spell).to eq(%w(G A B C D E F#))
+      end
+    end
+
+    context 'D Major' do
+      subject { described_class.new(root: 'D', number: 2741) }
+
+      it 'spells the scale' do
+        expect(subject.spell).to eq(%w(D E F# G A B C#))
       end
     end
   end
